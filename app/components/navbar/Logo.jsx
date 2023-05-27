817d4cfc0c0f36a6056754813c96d70720311085
Sandeep <Sandeep1dimri@gmail.com>
2023-05-26 23:45:12 -0400

    upto logo

---
+++ app/components/navbar/Logo.jsx
@@ -0,0 +1,24 @@
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+function Logo(){
+    const router=useRouter();
+    return(
+        <Image
+        alt="Logo"
+        className='hidden md:block cursor-pointer'
+        height="100"
+        width="100"
+        src='/images/logo.png'
+        >
+
+        </Image>
+
+
+    );
+
+
+}
+
+export default Logo;
